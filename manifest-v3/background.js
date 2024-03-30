5b876b3f918c0b896ba55dfc9816853447a9b1fb
INSANEMODE <scottshome02@gmail.com>
2024-03-30 03:32:05 -0500

    - remove permission option from options menu - add permission request into start page, to allow granting host permissions on first install, and to allow calling a tab from the background script with the url query `?permissions=1` when accessing twitter after denying the permissions, or upgrading from a previous version. - remove webNavigation listener on first setup to avoid opening permission request tab while trying to log into twitter -

--- manifest-v3/background.js
+++ manifest-v3/background.js
@@ -12,6 +12,27 @@ async function getPlatform(){
 };
 
 
+const permissions = {
+  origins: ["*://*.twitter.com/*", "*://*.x.com/*"],
+};
+const filter = {
+  url: [{ hostContains: "twitter.com",  }, { hostContains: "x.com" }],
+};
+async function checkHostPermissions(details) {
+  if (details.url.includes("oauth2")) {
+    // They are logging in to Twitter for Soupcan, so they're
+    // probably in the setup. Don't pop open a new setup window.
+    return;
+  }
+  if(!await browser.permissions.contains(permissions)){
+    browser.tabs.create({
+      url: getURL('start.html?permissions=1')
+    });
+
+  }
+}
+browser.webNavigation.onCompleted.addListener(checkHostPermissions, filter);
+
 function iOS() {
   return [
     "ios"
